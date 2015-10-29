a100ad928c8d076261e34f6f5c8ad69ba85eca31
imwgy <imwgy@hotmail.com>
2015-10-29 14:34:34 +0800

    Update UpsertCatagory.sql
    添加插入bsp_navs功能

--- StorePrecedures/UpsertCatagory.sql
+++ StorePrecedures/UpsertCatagory.sql
@@ -1,3 +1,5 @@
+--UpsertCatagory 'test_UpsertCatagory','大 家 电',3,0
+
 IF OBJECT_ID ( 'UpsertCatagory', 'P' ) IS NOT NULL 
     DROP PROCEDURE UpsertCatagory;
 GO
@@ -12,19 +14,48 @@ BEGIN
 	DECLARE 
 	@cateid smallint=null,
 	@parentid smallint=null,
-	@nextid smallint=null
-
+	@nextid smallint=null,
+	@nextnavid smallint=null
+	
+	-- new add by wgy -------------------------------
+	,@pid smallint=null
+	-------------------------------------------------
+	
+                                                                                                                                                                                                                                             
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	SET IDENTITY_INSERT 'bsp_categories' ON;
+	--SET IDENTITY_INSERT 'bsp_categories' ON;
 
    select @cateid=cateid from bsp_categories where LTRIM(RTRIM(name))=@name;
    if(@cateid is null)
 	   begin
-		select @nextid =IDENT_CURRENT('bsp_categories');
+		select @nextid =IDENT_CURRENT('bsp_categories')+1;
 		select @parentid=cateid from bsp_categories where LTRIM(RTRIM(name))=@parentName;
-		INSERT INTO bsp_categories(cateid,name,parentid,layer,haschild,path) Values(@nextid,@name,@parentid,@layer,@haschild,@parentid+','+@nextid);
+		
+		--INSERT INTO bsp_categories(name,displayorder,parentid,layer,haschild,path) Values(@name,0,@parentid,@layer,@haschild,@parentid+','+@nextid);
+		INSERT INTO bsp_categories(name,displayorder,parentid,layer,haschild,[path]) Values(@name,0,@parentid,@layer,@haschild,cast(@parentid as varchar(10))+','+cast(@nextid as varchar(10)));
+		
+		
+		-- new add 插入 navs 表数据 ------------------------------------
+		--这里假设nav和categories 名称一致,或者用url里包含的cateid来查找？
+		select @pid=[id] from bsp_navs where LTRIM(RTRIM(name))=@parentName;
+		--select @pid=@parentid  from bsp_navs where  url = '/list/'+@cateid+'.html'
+		
+		if(@pid is not null)
+   			begin
+				INSERT INTO bsp_navs([pid],[layer],[name],[title],[url],[target],[displayorder]) Values(@pid,@layer,@name,'title','/list/'+cast(@nextid as varchar(10))+'.html',0,0);
+				--INSERT INTO bsp_navs([pid],[layer],[name],[title],[url],[target],[displayorder]) Values(@pid,@layer,@name,'title','/list/'+'.html',0,0);
+			end
+		else
+			begin
+				--在没有找到navs表父节点的情况下如何插入？
+				select @nextnavid =IDENT_CURRENT('bsp_navs');
+				--INSERT INTO bsp_navs([pid],[layer],[name],[title],[url],[target],[displayorder]) Values(0,1,@name,'title','/list/'+cast(@cateid as varchar(10))+'.html',0,0);
+			
+			end
+		----------------------------------------------------------------
+			
 		end
    else
 	begin
@@ -32,9 +63,24 @@ BEGIN
 		UPDATE bsp_categories SET
 		parentid=coalesce(@parentid, parentid),
 		layer=coalesce(@layer,layer),
-		haschild=coalesce(@haschild,haschild),
-		path=coalesce(@parentid+','+@nextid,path)
+		haschild=coalesce(@haschild,haschild)
+		,path=coalesce(@parentid+@nextid,path)
 		WHERE name=@name;
+		
+		
+		-- new add 更新 navs 表数据 ------------------------------------
+		--这里假设nav和categories 名称一致,或者用url里包含的cateid来查找？
+		select @pid=@parentid from bsp_navs where LTRIM(RTRIM(name))=@name;
+		if(@pid is not null)
+   			begin
+				UPDATE bsp_navs
+				   SET [pid] = @pid
+					  ,[layer] = coalesce(@layer,layer)
+					  ,[url] = '/list/'+@cateid+'.html'
+				WHERE  LTRIM(RTRIM(name))=@name;
+			end
+		----------------------------------------------------------------
+		
 	end
 END
 GO
